3440b49455feeaf96d453a5907d47a5c6b0b02c1
blue-calm <nellija.hamane@outlook.com>
2020-03-04 15:28:42 +0200

    connection test

--- HuffmanBlackStorks/src/Huffman.java
+++ HuffmanBlackStorks/src/Huffman.java
@@ -5,5 +5,5 @@ public class Huffman {
 			// zapyskaet gui
 	}
 		//TODO write stuffjlkj
-		//TODO remember your user name & password  dchfgh hdsfoih
+		//TODO remember your user name & password  dchfgh hdsfoinknh
 }
