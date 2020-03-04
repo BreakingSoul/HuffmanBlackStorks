8feaa11e98bd265c192cab37195e6a3098d02790
BreakingSoul <danox1994@gmail.com>
2020-03-04 15:01:05 +0200

    Node + getAmount

--- HuffmanBlackStorks/src/Huffman.java
+++ HuffmanBlackStorks/src/Huffman.java
@@ -5,4 +5,25 @@ public class Huffman {
 			// zapyskaet gui
 	}
 
+	static void compress(String s) {
+		 
+		int[] freq = getAmount(s);
+		generateTree(freq);
+		
+	}
+	
+	static int[] getAmount(String s) {
+		final int ASCII_AMOUNT = 256;
+		 int[] freq = new int[ASCII_AMOUNT];
+		 for (char c : s.toCharArray()) {
+			 freq[c]++;
+		 }
+		 return freq;
+	}
+	
+	static void generateTree(int[] freq) {
+		
+	}
+	
+	
 }
