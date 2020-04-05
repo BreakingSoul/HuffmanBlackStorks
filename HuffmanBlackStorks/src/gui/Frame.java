2f65a199edf951e621c82ebb20999a5831138d6e
BreakingSoul <danox1994@gmail.com>
2020-04-05 16:28:47 +0300

    Added RLE decode.

--- HuffmanBlackStorks/src/gui/Frame.java
+++ HuffmanBlackStorks/src/gui/Frame.java
@@ -3,6 +3,7 @@ package gui;
 import logic.*;
 
 import java.awt.EventQueue;
+import java.awt.Graphics2D;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -14,7 +15,7 @@ import java.io.IOException;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.xml.bind.DatatypeConverter;
 
-import com.sun.javafx.scene.paint.GradientUtils.Point;
+//import com.sun.javafx.scene.paint.GradientUtils.Point;
 
 //import com.sun.org.apache.xerces.internal.impl.dv.util.Base64;
 
@@ -278,16 +279,12 @@ public class Frame extends javax.swing.JFrame {
 						System.out.println(encoded);
 						// save encoded in a file
 
-						
-						
 					} else if (Huffmanes.isSelected() && Decodes.isSelected()) {
 						// saves Huffman decoded text as regular file
 						String decoded = Huffman.decompress(textArea.getText());
 						System.out.println(decoded);
 						// save decoded in a file
 
-						
-						
 					} else if (RLEs.isSelected() && Encodes.isSelected()) {
 						// saves RLE encoded text as whatever eats less space (needs for next labdarba)
 						String encoded = RLE.compress(textArea.getText());
@@ -295,23 +292,24 @@ public class Frame extends javax.swing.JFrame {
 						// save encoded in a file
 						JFileChooser fileChooser = new JFileChooser(new File("c:\\"));
 						fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-						fileChooser.setDialogTitle("Save file"); 
-						fileChooser.setFileFilter(new Filter(".txt", "Text File")); 
-						fileChooser.setFileFilter(new Filter(".docx", "Microsoft Word Documents")); 
+						fileChooser.setDialogTitle("Save file");
+						fileChooser.setFileFilter(new Filter(".txt", "Text File"));
+						fileChooser.setFileFilter(new Filter(".docx", "Microsoft Word Documents"));
 						int result = fileChooser.showSaveDialog(null);
-						if (result == JFileChooser.APPROVE_OPTION) { String content = textArea.getText(); File fi = fileChooser.getSelectedFile(); 
-						try { FileWriter fw = new FileWriter(fi.getPath()); 
-						fw.write(encoded); 
-						fw.flush(); 
-						fw.close();
-						} catch (Exception e2) { JOptionPane.showMessageDialog(null, e2.getMessage());
-						  
-						}
-						  
+						if (result == JFileChooser.APPROVE_OPTION) {
+							File fi = fileChooser.getSelectedFile();
+							try {
+								FileWriter fw = new FileWriter(fi.getPath());
+								fw.write(encoded);
+								fw.flush();
+								fw.close();
+							} catch (Exception e2) {
+								JOptionPane.showMessageDialog(null, e2.getMessage());
+
+							}
+
 						}
 
-						
-						
 					} else if (RLEs.isSelected() && Decodes.isSelected()) {
 						// saves RLE decoded text as regular file (needs for next labdarba)
 						String decoded = RLE.decompress(textArea.getText());
@@ -319,23 +317,23 @@ public class Frame extends javax.swing.JFrame {
 						// save decoded in a file
 						JFileChooser fileChooser = new JFileChooser(new File("c:\\"));
 						fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-						fileChooser.setDialogTitle("Save file"); 
-						fileChooser.setFileFilter(new Filter(".txt", "Text File")); 
-						fileChooser.setFileFilter(new Filter(".docx", "Microsoft Word Documents")); 
+						fileChooser.setDialogTitle("Save file");
+						fileChooser.setFileFilter(new Filter(".txt", "Text File"));
+						fileChooser.setFileFilter(new Filter(".docx", "Microsoft Word Documents"));
 						int result = fileChooser.showSaveDialog(null);
-						if (result == JFileChooser.APPROVE_OPTION) { String content = textArea.getText(); File fi = fileChooser.getSelectedFile(); 
-						try { FileWriter fw = new FileWriter(fi.getPath()); 
-						fw.write(decoded); 
-						fw.flush(); 
-						fw.close();
-						} catch (Exception e2) { JOptionPane.showMessageDialog(null, e2.getMessage());
-						  
-						}
-						  
+						if (result == JFileChooser.APPROVE_OPTION) {
+							File fi = fileChooser.getSelectedFile();
+							try {
+								FileWriter fw = new FileWriter(fi.getPath());
+								fw.write(decoded);
+								fw.flush();
+								fw.close();
+							} catch (Exception e2) {
+								JOptionPane.showMessageDialog(null, e2.getMessage());
+							}
+
 						}
 
-						
-						
 					}
 
 					/*
@@ -363,10 +361,7 @@ public class Frame extends javax.swing.JFrame {
 						String encoded = Huffman.compress(textArea.getText());
 						System.out.println(encoded);
 						// save encoded in a file
-						
 
-						
-						
 					} else if (Huffmanes.isSelected() && Decodes.isSelected()) {
 						// saves Huffman decoded text as bmp (for next labdarba)
 						String decoded = Huffman.decompress(textArea.getText());
@@ -374,60 +369,78 @@ public class Frame extends javax.swing.JFrame {
 						decoded = toARGB(decoded);
 						// save decoded in a file
 
-						
-						
-						
 					} else if (RLEs.isSelected() && Encodes.isSelected()) {
-						// Saves RLE encoded picture as whatever takes less memory.
+						// Saves RLE encoded picture.
+						
 						String encoded = RLE.compressPicture(textArea.getText());
-						System.out.println(encoded);
+		//				System.out.println(encoded);
 						// save encoded in a file
 						JFileChooser fileChooser = new JFileChooser(new File("c:\\"));
 						fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
-						fileChooser.setDialogTitle("Save file"); 
+						fileChooser.setDialogTitle("Save file");
 						int result = fileChooser.showSaveDialog(null);
-						if (result == JFileChooser.APPROVE_OPTION) { String content = textArea.getText(); File fi = fileChooser.getSelectedFile(); 
-						try { FileWriter fw = new FileWriter(fi.getPath()); 
-						fw.write(encoded); 
-						fw.flush(); 
-						fw.close();
-						} catch (Exception e2) { JOptionPane.showMessageDialog(null, e2.getMessage());
-						  
+						if (result == JFileChooser.APPROVE_OPTION) {
+							File fi = fileChooser.getSelectedFile();
+							try {
+								FileWriter fw = new FileWriter(fi.getPath());
+								fw.write(encoded);
+								fw.flush();
+								fw.close();
+							} catch (Exception e2) {
+								JOptionPane.showMessageDialog(null, e2.getMessage());
+
+							}
+
 						}
-						  
-						}
-						
-						
-						
-						
+
 					} else if (RLEs.isSelected() && Decodes.isSelected()) {
-						// Saves decoded picture as bmp.
+						// Saves decoded picture.
+
 						String decoded = RLE.decompressPicture(textArea.getText());
-						System.out.println(decoded);
-						decoded = toARGB(decoded);
-						byte[] bytes =  DatatypeConverter.parseBase64Binary(decoded);//длина массива кастрированна, но на картике появляется контур
-						//byte[] bytes = decoded.getBytes(); сохраняет длину массива, но картинка пустая
-						//byte[] bytes = {0xa,0x2,0xf,(byte)0xff,(byte)0xff,(byte)0xff}; как это было в примере с сохранением картинки
-						//System.out.println(bytes);
-						int width = 16;
-						int height = 16;
-						DataBuffer buffer = new DataBufferByte(bytes, bytes.length);
-
-						//3 bytes per pixel: red, green, blue
-						WritableRaster raster = Raster.createInterleavedRaster(buffer, width, height, 3 * width, 3, new int[] {0, 1, 2}, null);
-						ColorModel cm = new ComponentColorModel(ColorModel.getRGBdefault().getColorSpace(), false, true, Transparency.OPAQUE, DataBuffer.TYPE_BYTE); 
-						BufferedImage image = new BufferedImage(cm, raster, true, null);
-
-						try {
-							System.out.println(Arrays.toString(bytes));
-							ImageIO.write(image, "png", new File("image.png"));//сохраняет в папке проекта
-						} catch (IOException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
+						int[] intPixels = new int[decoded.length()];
+						for (int i = 0; i < decoded.length(); i++) {
+							if (decoded.charAt(i) == '0') {
+								intPixels[i] = -1;
+							} else {
+								intPixels[i] = -16777216;
+							}
 						}
-						
-						
-						
+
+						int width = (int) Math.sqrt((double) intPixels.length);
+						int height = (int) Math.sqrt((double) intPixels.length);
+
+						BufferedImage intImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_ARGB);
+						Graphics2D g2d = intImage.createGraphics();
+						Color white = new Color(-1, true);
+						Color black = new Color(-16777216, true);
+						int counter = 0;
+
+						for (int i = 0; i < height; i++) {
+							for (int j = 0; j < width; j++) {
+								if (intPixels[counter] == -1) {
+									g2d.setColor(white);
+								} else {
+									g2d.setColor(black);
+								}
+								g2d.fillRect(j, i, 2, 2);
+								counter++;
+							}
+						}
+						g2d.dispose();
+
+						JFileChooser fileChooser = new JFileChooser(new File("c:\\"));
+						fileChooser.setFileSelectionMode(JFileChooser.FILES_ONLY);
+						fileChooser.setDialogTitle("Save file");
+						fileChooser.setFileFilter(new Filter(".png", "Image"));
+						int result = fileChooser.showSaveDialog(null);
+						if (result == JFileChooser.APPROVE_OPTION) {
+							File fi = fileChooser.getSelectedFile();
+							try {
+								ImageIO.write(intImage, "png", fi);
+							} catch (IOException e) {
+								e.printStackTrace();
+							}
+						}
 					}
 
 				} else {
@@ -439,13 +452,12 @@ public class Frame extends javax.swing.JFrame {
 
 		btnNewButton.setBounds(141, 225, 153, 23);
 		frame.getContentPane().add(btnNewButton);
-		
+
 		JButton btnNewButton_3 = new JButton("Open RLE'd image");
 		btnNewButton_3.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				JFileChooser fileChooser = new JFileChooser(new File("c:\\"));
 				fileChooser.setDialogTitle("Unzip Picture");
-	//			fileChooser.setFileFilter(new Filter(".docx", "Microsoft Word Documents"));
 				fileChooser.setFileFilter(new Filter(".txt", "Text File"));
 				int result = fileChooser.showOpenDialog(null);
 				if (result == JFileChooser.APPROVE_OPTION) {
@@ -474,7 +486,7 @@ public class Frame extends javax.swing.JFrame {
 		btnNewButton_3.setBounds(10, 69, 146, 25);
 		btnNewButton_3.setBackground(Color.CYAN);
 		frame.getContentPane().add(btnNewButton_3);
-		
+
 		/*
 		 * JButton btnNewButton_2 = new JButton("Unzip");
 		 * btnNewButton_2.addActionListener(new ActionListener() { public void
@@ -499,7 +511,7 @@ public class Frame extends javax.swing.JFrame {
 		 * frame.getContentPane().add(btnNewButton_2);
 		 */
 	}
-	
+
 	public static String toARGB(String s) {
 		s = s.replace("0", "z");
 		s = s.replace("1", "11111111000000000000000000000000");
@@ -507,3 +519,25 @@ public class Frame extends javax.swing.JFrame {
 		return s;
 	}
 }
+
+// System.out.println(decoded);
+// decoded = toARGB(decoded);
+// byte[] bytes = DatatypeConverter.parseBase64Binary(decoded);//длина массива
+// кастрированна, но на картике появляется контур
+// byte[] bytes = decoded.getBytes(); сохраняет длину массива, но картинка
+// пустая
+// byte[] bytes = {0xa,0x2,0xf,(byte)0xff,(byte)0xff,(byte)0xff}; как это было в
+// примере с сохранением картинки
+// System.out.println(bytes);
+
+// System.out.println(Arrays.toString(bytes));
+// System.out.println(width + " " + height);
+// DataBuffer buffer = new DataBufferByte(bytes, bytes.length);
+
+// 3 bytes per pixel: red, green, blue
+// WritableRaster raster = Raster.createInterleavedRaster(buffer, width, height,
+// 3 * width, 3, new int[] {0, 1, 2}, null);
+// ColorModel cm = new
+// ComponentColorModel(ColorModel.getRGBdefault().getColorSpace(), false, true,
+// Transparency.OPAQUE, DataBuffer.TYPE_BYTE);
+// BufferedImage image = new BufferedImage(cm, raster, true, null);
